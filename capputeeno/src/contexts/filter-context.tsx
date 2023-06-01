fd645dbe285761ab4e05079bf34f2348a7cd9dea
Alexandre Oliveira <serolfalexandre@gmail.com>
2023-06-01 17:01:29 -0300

    feat: Initial products fetch

--- capputeeno/src/contexts/filter-context.tsx
+++ capputeeno/src/contexts/filter-context.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import { FilterType } from "@/types/filter-types";
+import { PriorityType } from "@/types/priority-types";
 import { ReactNode, createContext, useState } from "react";
 
 export const FilterContext = createContext({
   search: '',
   page: 0,
   type: FilterType.ALL,
+  priority: PriorityType.NEWS,
+  setPriority: (value: PriorityType) => {},
   setSearch: (value: string) => {},
   setPage: (value: number) => {},
   setType: (value: FilterType) => {},
@@ -21,8 +24,10 @@ export function FilterContextProvider({ children }: ProviderProps) {
   const [search, setSearch] = useState('')
   const [page, setPage] = useState(0)
   const [type, setType] = useState(FilterType.ALL)
+  const [priority, setPriority] = useState(PriorityType.NEWS)
+
   return(
-    <FilterContext.Provider value={{search, page, type, setSearch, setPage, setType}}>
+    <FilterContext.Provider value={{search, page, type, setSearch, setPage, setType, priority, setPriority}}>
         {children}
     </FilterContext.Provider>
   )
