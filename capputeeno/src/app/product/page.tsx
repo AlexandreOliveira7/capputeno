ca8479dcf2beb55b2b9d3db636d317b2cf84a497
Alexandre Oliveira <serolfalexandre@gmail.com>
2023-06-07 21:35:48 -0300

    feat: add btn add to cart

--- capputeeno/src/app/product/page.tsx
+++ capputeeno/src/app/product/page.tsx
@@ -95,7 +95,32 @@ const ProductInfo = styled.div`
 export default function Product({ searchParams }: { searchParams: { id: string }}) {
   const { data } = useProduct(searchParams.id);
 
-  console.log(data)
+  const handleAddToCart = () => {
+    let cartItems = localStorage.getItem('cart-items');
+    if(cartItems) {
+        let cartItemsArray = JSON.parse(cartItems);
+
+        let existingProductIndex = cartItemsArray.findIndex((item: { id: string;}) => item.id === searchParams.id);
+
+        if(existingProductIndex != -1){
+            cartItemsArray[existingProductIndex].quantity =+ 1;
+        } else {
+            cartItemsArray.push({ ...data, quantity: 1, id: searchParams.id })
+        }
+
+        localStorage.setItem('cart-items', JSON.stringify(cartItemsArray))
+    } else {
+        const newCart = [
+            {
+                ...data,
+                id: searchParams.id,
+                quantity: 1
+            }
+        ]
+        localStorage.setItem('cart-items', JSON.stringify(newCart));
+    }
+    
+  }
   
   return(
     <DefaultPageLayout>
@@ -114,7 +139,7 @@ export default function Product({ searchParams }: { searchParams: { id: string }
                 <p>{data?.description}</p>
                </div>
             </ProductInfo>
-            <button>
+            <button onClick={handleAddToCart}>
               <ShoppingBagIcon />
               adicionar ao carrinho
               </button>
