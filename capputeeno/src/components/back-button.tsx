17db6b08e97da1f43c1fd70accbf50b05c5ebc7b
Alexandre Oliveira <serolfalexandre@gmail.com>
2023-06-10 21:46:32 -0300

    feat: creating cart page

--- capputeeno/src/components/back-button.tsx
+++ capputeeno/src/components/back-button.tsx
@@ -4,18 +4,18 @@ import { useRouter } from 'next/navigation';
 
 
 const Button = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 8px;
-  background: transparent;
-  border: none;
-  cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    background: transparent;
+    border: none;
+    cursor: pointer;
 
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 150%;
-  color: var(--secundary-text);
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 150%;
+    color: var(--secondary-text);
 `
 interface BtnProps{
   navigate: string;
